4beae8b1a06a35844961d9dbf15f441eb8dff003
Brian Moriguchi <brian@beckos.com>
2022-08-02 16:10:36 -0700

    Initial Commit

---
+++ GI_Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GI_Assignment
+//
+//  Created by Tatsuya Moriguchi on 8/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
